e748c70e8b9e67a2eec8199b7f4409faf1f9e1ce
Unknown <pagejakob@gmail.com>
2018-04-10 16:53:44 +1000

    v1 Base Boilerplate

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(
+  <p>Jakob Page Boilerplate - Welcome Back</p>,
+  document.getElementById("app")
+);
